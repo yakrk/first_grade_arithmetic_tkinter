d65d12cf01fac61fa3347ee719caf34bc6bd421f
yusuke <yusuke.takada@gmail.com>
2023-02-28 16:08:49 +0900

    - added remaining life feature - added images

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 import random
+from PIL import Image, ImageTk
 
 # Create a view with Tkinter
 BACKGROUND_COLOR = "white"
@@ -12,17 +13,33 @@ set_font = 'C:\Windows\Fonts\Arial.ttf'
 entered_text = ""
 question = None
 score = 0
+is_game_on = True
 
 # Level Settings
-seconds = 60
-arithmatic_choice = ["+","-"]
-min_num = 10
-max_num = 40
+SECONDS = 60
+ARITHMATIC_CHOICE = ["+","-"]
+MIN_NUM = 10
+MAX_NUMM = 40
+MAX_LIFE = 10
+remaining_life = MAX_LIFE
+life_list = []
+
+# Prep Images
+start_image = tk.PhotoImage(file = r"image\start.png")
+correct_image = tk.PhotoImage(file = r"image\correct.png")
+boo_image = tk.PhotoImage(file = r"image\boo.png")
+gameover_image = tk.PhotoImage(file = r"image\crying.png")
+ball_image = tk.PhotoImage(file = r"image\ball.png")
+start_resized_image = start_image.subsample(6, 6)
+correct_resized_image = correct_image.subsample(6, 6)
+boo_resized_image = boo_image.subsample(6, 6)
+gameover_resized_image = gameover_image.subsample(2,2)
+ball_resized_image = ball_image.subsample(20,20)
 
 #Timer class
 class Timer:
     def __init__(self):
-        self.count = seconds
+        self.count = SECONDS
         self.count_down()
         self.timer = None
 
@@ -33,25 +50,24 @@ class Timer:
             self.count -= 1
             self.timer = window.after(1000, self.count_down)
         else:
-            remaining_time.config(text="ゲームオーバー！！")
-            text_entry.pack_forget()
-            restart_btn.pack()
+            game_over()
         window.update()
 
     def reset_countdown(self):
         window.after_cancel(self.timer)
-        self.count = seconds
+        self.count = SECONDS
         self.count_down()
 
 # Create question class
 class Question:
     def __init__(self) -> None:
-        self.min = min_num
-        self.max = max_num
+        self.min = MIN_NUM
+        self.max = MAX_NUMM
         self.first_num = None
         self.second_num = None
-        self.arithmatics = arithmatic_choice
+        self.arithmatics = ARITHMATIC_CHOICE
         self.arithmatic = None
+        self.create_question()
         
     def create_question(self):
         self.first_num = random.randint(self.min, self.max)
@@ -74,54 +90,97 @@ class Question:
                 answer = self.first_num - self.second_num
         return answer
 
+
+def show_question():
+    global question
+    question = Question()
+
+def game_over():
+    global is_game_on
+    restart_btn.place(x=150,y=150)
+    background_image.config(image=gameover_resized_image)
+    remaining_time.config(text="ゲームオーバー！！")
+    is_game_on = False
+    text_entry.place_forget()
+
 def restart():
+    global is_game_on
     global score
-    text_entry.pack()
+    text_entry.delete("1.0","end")
+    text_entry.place(x=10,y=120)
     score = 0
+    is_game_on = True
+    reset_life()
     score_label.config(text = f"Score: {score}")
+    background_image.config(image=start_resized_image)
     countdown.reset_countdown()
-    restart_btn.pack_forget()
-    main()
+    restart_btn.place_forget()
+
+def reset_life():
+    global remaining_life
+    remaining_life = MAX_LIFE
+    for i in range(remaining_life):
+        xcor = 10 + 30*i
+        i = tk.Label(image=ball_resized_image, background=BACKGROUND_COLOR)
+        i.place(x=xcor, y=45)
+        life_list.append(i)
     
 def type_event(event):
     global score
-    result=text_entry.get(1.0, tk.END+"-1c")
-    answer = question.get_answer()
-    if result == "":
-        pass
-    elif int(result) == answer:
-        countdown.reset_countdown()
+    global life_list
+    global is_game_on
+    if is_game_on:
+        result=text_entry.get(1.0, tk.END+"-1c")
+        answer = question.get_answer()
+        # if correct
+        if int(result) == answer:
+            countdown.reset_countdown()
+            score += 1
+            score_label.config(text = f"Score: {score}")
+            background_image.config(image=correct_resized_image)
+            show_question()
+        # if wrong
+        else:
+            if len(life_list)<1:
+                game_over()
+            else:
+                for i, value in enumerate(life_list):
+                    if i == len(life_list)-1:
+                        value.destroy()
+                del life_list[-1]
+                background_image.config(image=boo_resized_image)
         text_entry.delete("1.0","end")
-        score += 1
-        score_label.config(text = f"Score: {score}")
-        main()
+
 
 # Show score
 score_label = tk.Label(text="Score: 0", background=BACKGROUND_COLOR, font=("Arial", 12))
-score_label.pack()
 
 # Create remaining time label
 remaining_time = tk.Label(
     text="", background=BACKGROUND_COLOR, font=("Arial", 12))
-remaining_time.pack()
-
 # Show entered text
 question_label = tk.Label(text="もんだい", background=BACKGROUND_COLOR, anchor="w",
                       width=400, wraplength=700, justify="left", font=("Arial", 18), foreground="#000000")
-question_label.pack()
-text_entry = tk.Text(width=400, height=2, background=BACKGROUND_COLOR, font=("Arial", 20))
-text_entry.pack()
-restart_btn = tk.Button(command=restart, text="リスタート")
+text_entry = tk.Text(width=20, height=2, background=BACKGROUND_COLOR, font=("Arial", 20))
+restart_btn = tk.Button(command=restart, text="スタート")
+# Create a photoimage object of the image in the path
+background_image = tk.Label(image=start_resized_image, background=BACKGROUND_COLOR)
 
-def main():
-    global question
-    question = Question()
-    question.create_question()
+# Place images
+score_label.place(x=150, y=0)
+remaining_time.place(x=100,y=20)
+question_label.place(x=10,y=80)
+text_entry.place(x=10,y=120)
+background_image.place(x=50,y=200)
 
-event_sequence = '<KeyPress>'
+# catch press Enter key 
+event_sequence = '<Return>'
 window.bind(event_sequence, type_event)
 
+#start upon launch
 countdown = Timer()
-main()
+reset_life()
+show_question()
+
 
 window.mainloop()
